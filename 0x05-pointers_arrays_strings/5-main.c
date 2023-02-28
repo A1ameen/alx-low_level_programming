6604a716d2d3ee9a087a8c12f8fdad012b7947d8
A1ameen <kharyourdeylarry@gmail.com>
2023-02-27 16:52:05 -0800

    Commikt message

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mai8n - check the code
+ *
+ * Return: Always o.
+ */
+
+int main(void)
+{
+	char s[10] = "My School";
+
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
+}
